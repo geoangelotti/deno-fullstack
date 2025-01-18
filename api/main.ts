615c0591336bdd143a0bb6ddedb9f07b7764edbb
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-01-18 16:17:46 +0100

    feat(api): add endpoints for dinosaur data

--- api/main.ts
+++ api/main.ts
@@ -1,4 +1,27 @@
 import { Hono } from "@hono/hono";
-
+import data from "./data.json" with { type: "json" };
 const app = new Hono();
+
+app.get("/", (c) => {
+	return c.text("Welcome to the dinosaur API!");
+  });
+  
+  app.get("/api/dinosaurs", (c) => {
+	return c.json(data);
+  });
+  
+  app.get("/api/dinosaurs/:dinosaur", (c) => {
+	if (!c.req.param("dinosaur")) {
+	  return c.text("No dinosaur name provided.");
+	}
+  
+	const dinosaur = data.find((item) =>
+	  item.name.toLowerCase() === c.req.param("dinosaur").toLowerCase()
+	);
+  
+	console.log(dinosaur);
+
+	return dinosaur ? c.json(dinosaur) : c.notFound();
+  });
+
 Deno.serve(app.fetch);
